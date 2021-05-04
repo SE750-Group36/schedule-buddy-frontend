231b6f994cfc5af334a659a8ddd154aee0c64496
Andrew Donovan <andrew.donovan12@outlook.co.nz>
2021-05-05 11:27:12 +1200

    Finished break time generation

--- schedulinator-frontend/src/PreferencesModal.tsx
+++ schedulinator-frontend/src/PreferencesModal.tsx
@@ -1,10 +1,12 @@
-import { Button, Divider, Fade, FormControl, FormControlLabel, FormLabel, makeStyles, Modal, Paper, Radio, RadioGroup, TextField } from '@material-ui/core';
+import { Button, Divider, Fade, FormControl, FormControlLabel, FormLabel, IconButton, makeStyles, Modal, Paper, Radio, RadioGroup, TextField } from '@material-ui/core';
 import { KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers';
 import { FunctionComponent, useState } from 'react'; 
 import DateFnsUtils from '@date-io/date-fns';
 import set from 'date-fns/set';
-import { ReplyAll } from '@material-ui/icons';
-import setDate from 'date-fns/setDate';
+import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DoneIcon from '@material-ui/icons/Done';
+import { format } from 'date-fns';
 
 enum Repeat {
   Never,
@@ -61,6 +63,23 @@ const useStyles = makeStyles((theme) => ({
   radio : {
     marginBottom: '20px',
     marginRight : '30px'
+  },
+  breakTime : {
+    marginBottom: '10px',
+    paddingLeft : '5px',
+    paddingRight : '5px',
+    display : 'flex',
+    alignItems : 'center',
+    justifyContent : 'space-between',
+    width : '100%'
+  },
+  prefSettings : {
+    marginBottom: '20px',
+    display : 'flex'
+  },
+  dailyTimes : {
+    display : 'flex',
+    flexDirection : 'column'
   }
 }));
 
@@ -69,7 +88,7 @@ export const PreferencesModal: FunctionComponent = () => {
   const [open, setOpen] = useState<boolean>(true)
   const [repeats, setRepeats] = useState<Repeat>(Repeat.Never);
   const [breakStart, setBreakStart] = useState<Date>(set(new Date(), {hours: 12, minutes: 0}));
-  const [breakEnd, setBreakEnd] = useState<Date>(set(new Date(), {hours: 1, minutes: 0}));
+  const [breakEnd, setBreakEnd] = useState<Date>(set(new Date(), {hours: 13, minutes: 0}));
   const [breakDate, setBreakDate] = useState<Date>(new Date());
   const classes = useStyles();
 
@@ -83,6 +102,7 @@ export const PreferencesModal: FunctionComponent = () => {
   } 
 
   const createBreak = () => {
+    console.log(breakDate)
     const breakTime : BreakTime = {
       breakDate,
       repeats,
@@ -109,6 +129,38 @@ export const PreferencesModal: FunctionComponent = () => {
     }
   }
 
+  const deleteBreak = (index: number) => {
+    var breakTimes = preferences.blockedTimes;
+    breakTimes.splice(index, 1);
+    setPreferences({...preferences, blockedTimes: breakTimes})
+  }
+
+  const breakTimePaper = (breakTime: BreakTime, index : number) => {
+    var dateString = '';
+    switch(breakTime.repeats) {
+      case Repeat.Never:
+        dateString = dateString + format(breakTime.breakDate, 'iiii do MMMM');
+        break;
+      case Repeat.Daily:
+        dateString = dateString + 'Every Day';
+        break;
+      case Repeat.Weekly:
+        dateString = dateString + 'Every ' + format(breakTime.breakDate, 'iiii');
+        break;
+    }
+
+    var timeString = format(breakTime.breakStart, 'h:mmaaa') + ' - ' + format(breakTime.breakEnd, 'h:mmaaa')
+
+    return (
+      <Paper className={classes.breakTime} key={index} elevation={3} variant="outlined"> 
+        <FreeBreakfastIcon color="primary" style={{padding : '6px'}}/>
+        <span>{dateString}</span>
+        <span>{timeString}</span>
+        <IconButton onClick={() => deleteBreak(index)} children={<DeleteIcon fontSize="small" color="action"/>} color="inherit" style={{padding : '6px'}}></IconButton>
+      </Paper>
+    )
+  }
+
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Modal
@@ -119,12 +171,14 @@ export const PreferencesModal: FunctionComponent = () => {
         <Fade in={open}>
           <FormControl className={classes.paper}>
             <h3 className={classes.prefHor}>Scheduling Preferences</h3>
-            <span className={classes.prefVert}>
+            <div className={classes.prefSettings}>
               <KeyboardDatePicker className={classes.prefHor} size="small" variant="inline" label="Start Date" inputVariant="outlined" format={"iiii, do"} value={preferences.startDate} onChange={startDate => updateTypeReferenceNode('startDate', startDate)} />
-              <KeyboardTimePicker className={classes.prefHor} minutesStep={15} size="small" variant="inline" label="Daily Start Time" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyStartTime} onChange={startTime => updateTypeReferenceNode('dailyStartTime', startTime)} />
-              <KeyboardTimePicker className={classes.prefHor} minutesStep={15} size="small" variant="inline" label="Daily Finish Time" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyEndTime} onChange={endTime => updateTypeReferenceNode('dailyEndTime', endTime)}/>
-              <TextField type="number" variant="outlined" size="small" label="Task Interval (Minutes)" value={preferences.maxInterval} onChange={event => setPreferences({...preferences, maxInterval: parseInt(event.target.value)})}/>
-            </span>
+              <TextField type="number" className={classes.prefHor} variant="outlined" size="small" label="Task Interval (Minutes)" value={preferences.maxInterval} onChange={event => setPreferences({...preferences, maxInterval: parseInt(event.target.value)})}/>
+              <div className={classes.dailyTimes}>
+                <KeyboardTimePicker className={classes.prefVert} minutesStep={15} size="small" variant="inline" label="Daily Start Time" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyStartTime} onChange={startTime => updateTypeReferenceNode('dailyStartTime', startTime)} />
+                <KeyboardTimePicker minutesStep={15} size="small" variant="inline" label="Daily Finish Time" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyEndTime} onChange={endTime => updateTypeReferenceNode('dailyEndTime', endTime)}/>
+              </div>
+            </div>
 
             <Divider className={classes.prefVert}/>
 
@@ -135,20 +189,20 @@ export const PreferencesModal: FunctionComponent = () => {
                   <FormControlLabel checked={repeats==Repeat.Never} value={Repeat.Never} control={<Radio />} label="One Off" />
                   <FormControlLabel checked={repeats==Repeat.Daily} value={Repeat.Daily} control={<Radio />} label="Daily" />
                   <FormControlLabel className={classes.prefVert} checked={repeats==Repeat.Weekly} value = {Repeat.Weekly} control={<Radio />} label="Weekly" />
-                  <Button variant="contained" color="primary" onClick={() => createBreak()}>Add</Button>
+                  <Button variant="outlined" color="primary" onClick={() => createBreak()}>Add</Button>
                 </RadioGroup>
                 <div style={{display: 'flex', flexDirection: 'column', marginRight: '30px'}}>
-                  {repeats!=Repeat.Daily ? <KeyboardDatePicker className={classes.breakField} size="small" variant="inline" label="Date" inputVariant="outlined" format={"iiii, do"} value={preferences.startDate} onChange={date => date == null ? null : setBreakDate(date)} /> : ''}
-                  <KeyboardTimePicker className={classes.breakField} minutesStep={15} size="small" variant="inline" label="Start" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyStartTime} onChange={startTime => startTime == null ? null : setBreakStart(startTime)} />
-                  <KeyboardTimePicker className={classes.breakField} minutesStep={15} size="small" variant="inline" label="End" inputVariant="outlined" format={"h:mmaaa"} value={preferences.dailyStartTime} onChange={endTime => endTime == null ? null : setBreakEnd(endTime)} />
+                  {repeats!=Repeat.Daily ? <KeyboardDatePicker className={classes.breakField} size="small" variant="inline" label="Date" inputVariant="outlined" format={"iiii, do"} value={breakDate} onChange={date => date == null ? null : setBreakDate(date)} /> : ''}
+                  <KeyboardTimePicker className={classes.breakField} minutesStep={15} size="small" variant="inline" label="Start" inputVariant="outlined" format={"h:mmaaa"} value={breakStart} onChange={startTime => startTime == null ? null : setBreakStart(startTime)} />
+                  <KeyboardTimePicker className={classes.breakField} minutesStep={15} size="small" variant="inline" label="End" inputVariant="outlined" format={"h:mmaaa"} value={breakEnd} onChange={endTime => endTime == null ? null : setBreakEnd(endTime)} />
                 </div>
               </div>
-              <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
-                {preferences.blockedTimes.map(breakTime => {
-                  return <Paper key={breakTime.breakEnd.valueOf()} elevation={3} variant="outlined"> {breakTime.breakEnd.toString()} </Paper>
-                })}
+              <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '400px'}}>
+                {preferences.blockedTimes.map((breakTime, index) => breakTimePaper(breakTime, index))}
               </div>
             </div>
+
+            <Button variant="contained" color="primary" style={{width: '120px', alignSelf: 'center', marginTop: '10px'}} onClick={() => setOpen(false)}>Done</Button>
           </FormControl>
         </Fade>
         
